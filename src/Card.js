5ee0304b54159568419b74419a3094d7cf527413
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-25 12:29:19 -0700

    Add base funcrtionality. Need to finish conditional on deck end.

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class Card extends Component {
     render() {
         return (
-            <div>
                 <img src={this.props.image} alt="game_card"></img>
-            </div>
         );
     }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Card from './Card';
 import Button from './Button';
+import uuid from 'uuid/v4';
+
 const DECK_OF_CARD_URL = `https://deckofcardsapi.com/api/deck`;
 
 class Deck extends Component {
@@ -10,25 +12,37 @@ class Deck extends Component {
         this.state = { 
           deckID: null,
           card: [],
-          button: "Draw a card!"
+          // FixMe better nomenclature for this and what is being passed to button.
+          button: "Draw a card!",
+          cardsLeft: null
         };
+        this.getCard = this.getCard.bind(this);
       }
 
     async componentDidMount(){
         let deckOfCards = await axios.get(`${DECK_OF_CARD_URL}/new/shuffle/?deck_count=1`);
-        this.setState( {deckID: deckOfCards.deck_id} );
+        this.setState( {deckID: deckOfCards.data.deck_id} );
     }
-      
+    
     async getCard(){
         let newCard = await axios.get(`${DECK_OF_CARD_URL}/${this.state.deckID}/draw/?count=1`);
-        const newState = newCard.cards[0].image;
-        this.setState ({ card: [...this.state.card, newState] });
+        const newState = newCard.data.cards[0].image;
+        const remaining = newCard.data.remaining;
+        this.setState ({ card: [...this.state.card, newState],
+                        cardsLeft: remaining });
     }
 
     render() {
+        if (this.state.cardsLeft === 0) {
+
+        }
+        const cards = this.state.card.map(c => {
+            // FixMe use deck code not uuid
+            return <Card key={uuid()} image={c}/>
+        })
         return (
             <div>
-                <Card image={this.card[0]}/>
+                <div>{cards}</div>
                 <Button handleCard={this.getCard} type={this.state.button}/>
             </div>
         );
